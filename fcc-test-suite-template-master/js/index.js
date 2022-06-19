eece7534f518694e8042fda42b5ffb96c7cbd0a4
tonnymuchui <tonykanyingah@gmail.com>
2022-06-19 19:05:08 +0300

    you're now a developer

--- fcc-test-suite-template-master/js/index.js
+++ fcc-test-suite-template-master/js/index.js
@@ -1,3 +1,16 @@
+//Select element function
+const selectElement = function (element){
+  return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle'); 
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+  body.classList.toggle('open');
+})
+
+
 // !! IMPORTANT README:
 
 // You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
